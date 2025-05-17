22a8f98e367d675c178924793aba46e01f52a64d
Alex <aprildistant@gmail.com>
2025-05-17 14:45:28 +0300

    Добавление Валидации и логики сброса. Связывание контролов формы с состояниями компонента.

--- client/src/components/Users/CreateUser.js
+++ client/src/components/Users/CreateUser.js
@@ -10,7 +10,20 @@ const CreateUser = (props) => {
 
     const createUserHandler = (event) => {
         event.preventDefault();
+
+        // Делаем проверки валидации до отправки данных. 
+        // Пока просто return, потому будет модальное окно.
+        if (inputName.trim().length === 0 || inputAge.trim().length === 0 || +inputAge < 1) {
+            return;
+        }
+        // if (+inputAge < 1) {
+        //     return;
+        // }
+
         console.log(inputName, inputAge);
+        // При этом надо инпуты сделать зависимыми от состояний компонента через value.
+        setInputName("");
+        setInputAge("");
     };
 
     const nameChangeHandler = (event) => {
@@ -25,10 +38,19 @@ const CreateUser = (props) => {
         <Card className={styles.input}>
             <form onSubmit={createUserHandler}>
                 <label htmlFor="name">Имя</label>
-                <input id="name" type="text" onChange={nameChangeHandler} />
+                <input
+                    id="name"
+                    type="text"
+                    onChange={nameChangeHandler}
+                    value={inputName} />
                 {/* onChange - возникнет только когда измененный элемент потеряет фокус */}
+
                 <label htmlFor="age">Возраст</label>
-                <input id="age" type="number" onChange={ageChangeHandler} />
+                <input
+                    id="age"
+                    type="number"
+                    onChange={ageChangeHandler}
+                    value={inputAge} />
                 {/* <button type="submit">Добавить Пользователя</button> */}
                 <Button type="submit">Добавить Пользователя</Button>
             </form>
