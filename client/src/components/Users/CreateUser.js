0bb0fec68adf8aa6b725cfe6ade05f9d3a6d772c
Alex <aprildistant@gmail.com>
2025-05-17 14:28:19 +0300

    Управление состоянием ввода данных (просто помещение данных в стейт)

--- client/src/components/Users/CreateUser.js
+++ client/src/components/Users/CreateUser.js
@@ -1,19 +1,34 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import styles from "./CreateUser.module.css";
 import Button from "../UI/Button";
 
 const CreateUser = (props) => {
+
+    const [inputName, setInputName] = useState("");
+    const [inputAge, setInputAge] = useState("");
+
     const createUserHandler = (event) => {
         event.preventDefault();
+        console.log(inputName, inputAge);
+    };
+
+    const nameChangeHandler = (event) => {
+        setInputName(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setInputAge(event.target.value);
     };
 
     return (
         <Card className={styles.input}>
             <form onSubmit={createUserHandler}>
                 <label htmlFor="name">Имя</label>
-                <input id="name" type="text" />
+                <input id="name" type="text" onChange={nameChangeHandler} />
+                {/* onChange - возникнет только когда измененный элемент потеряет фокус */}
                 <label htmlFor="age">Возраст</label>
-                <input id="age" type="number" />
+                <input id="age" type="number" onChange={ageChangeHandler} />
                 {/* <button type="submit">Добавить Пользователя</button> */}
                 <Button type="submit">Добавить Пользователя</Button>
             </form>
